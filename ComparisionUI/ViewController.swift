ec06f902588a7affc014d232483594a5695e2ae4
Sameer Poudel <samir@samirpdl.com.np>
2017-08-16 12:54:47 +0530

    Initial Commit

---
+++ ComparisionUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComparisionUI
+//
+//  Created by Sameer Poudel on 16/08/17.
+//  Copyright © 2017 Data Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
